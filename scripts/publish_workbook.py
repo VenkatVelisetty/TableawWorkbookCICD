996abb00a831dc9be1b940218075f1aa20fe900c
Venkateswarlu Velisetty <venkateswarlu.velisetty@invitationhomes.com>
2022-10-28 16:47:21 +0530

    Commit
    Commit
    a

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -5,13 +5,17 @@ import os
 
 
 def main(args):
-    project_data_json = json.loads(args.project_data)
+    project_data = json.loads(args.project_data)
     try:
         # Step 1: Sign in to server.
-        tableau_auth = TSC.TableauAuth(args.username, args.password)
+        print(project_data['site_id'])
+        tableau_auth = TSC.TableauAuth(
+            args.username, args.password,project_data['site_id'])
         server = TSC.Server(args.server_url)
-
+        project_data_json = project_data['workbooks']
         with server.auth.sign_in(tableau_auth):
+            site_item = server.sites.get_by_name('DataLab')
+            print(site_item.id, site_item.name, site_item.content_url, site_item.state)
             try:
                 for data in project_data_json:
                     wb_path = os.path.dirname(os.path.realpath(__file__)).rsplit(
