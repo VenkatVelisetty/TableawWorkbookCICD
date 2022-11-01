d6ae0b34bcc7a93bdbb6a9e8207aaf32f710c91d
VenkatVelisetty <114144376+VenkatVelisetty@users.noreply.github.com>
2022-11-01 17:24:02 +0530

    commenting other one

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -37,8 +37,8 @@ def main(args):
                         exit(1)
                     else:
                         # Step 2: Get all the projects on server, then look for the default one.
-                        project_id_by_name = get_project_id_by_path_with_tree(args, data['project_path'])
-                        print(project_id_by_name)
+                        # project_id_by_name = get_project_id_by_path_with_tree(args, data['project_path'])
+                        # print(project_id_by_name)
 
                         # Step 3: If default project is found, form a new workbook item and publish.
                         if project_id_by_name is not None:
