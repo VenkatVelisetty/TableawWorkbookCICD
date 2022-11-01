7e7ada6fd8835e814493890fcff84af5783f886d
VenkatVelisetty <114144376+VenkatVelisetty@users.noreply.github.com>
2022-11-01 17:09:50 +0530

    project_id_by_name

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -36,12 +36,11 @@ def main(args):
                         exit(1)
                     else:
                         # Step 2: Get all the projects on server, then look for the default one.
-                        project_id = get_project_id_by_path_with_tree(args, data['project_path'])
-			
-                        print(project_id)
+                        project_id_by_name = get_project_id_by_path_with_tree(args, data['project_path'])
+                        print(project_id_by_name)
 
                         # Step 3: If default project is found, form a new workbook item and publish.
-                        if project_id is not None:
+                        if project_id_by_name is not None:
                             new_workbook = TSC.WorkbookItem(
                                 name=data['name'], project_id=project_id, show_tabs=data['show_tabs'])
                             new_workbook = server.workbooks.publish(
